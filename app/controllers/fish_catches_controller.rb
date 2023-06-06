930403e6ce0986bcb0e726dfd51d6500bd062f5f
Bruno Leite <bmarianoleite3@gmail.com>
2023-06-06 18:20:41 -0300

    inline editing

--- app/controllers/fish_catches_controller.rb
+++ app/controllers/fish_catches_controller.rb
@@ -19,7 +19,7 @@ class FishCatchesController < ApplicationController
 
   def update
     if @fish_catch.update(fish_catch_params)
-      redirect_to tackle_box_item_for_catch(@fish_catch)
+      @fish_catches = fish_catches_for_bait(@fish_catch.bait)
     else
       render :edit, status: :unprocessable_entity
     end
