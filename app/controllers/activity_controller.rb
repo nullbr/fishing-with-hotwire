305189e6f679b40d482f0da559f66205ba1b2212
Bruno Leite <bmarianoleite3@gmail.com>
2023-06-14 17:00:30 -0300

    adding lazy load for activity infinite scroll

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -4,7 +4,7 @@ class ActivityController < ApplicationController
   before_action :require_signin
 
   def index
-    @fish_catches = FishCatch.order(created_at: :desc).includes(:user, :bait)
+    @pagy, @fish_catches = pagy(FishCatch.order(created_at: :desc).includes(:user, :bait), items: 5)
 
     @fish_catches = current_user.assign_my_likes_to_catches(@fish_catches)
   end
